d34fd4c94824e9a11d5e63eb270986f4d3911a95
Enweave <enweave@gmail.com>
2024-09-14 06:29:03 +0500

    spawn manager cpp health component god mode

---
+++ Source/BrackeyGame/Private/SpawnManager.cpp
@@ -0,0 +1,63 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpawnManager.h"
+
+USpawnBatch::USpawnBatch()
+{
+	// Default constructor implementation
+}
+
+
+ASpawnManager::ASpawnManager()
+{
+}
+
+void ASpawnManager::InitManager(const FUWavesConfig& InWavesConfig, const int32 StartWaveIndex,
+                                const int32 StartStageIndex)
+{
+	CurrentWaveIndex = StartWaveIndex;
+	CurrentStageIndex = StartStageIndex;
+
+	// Check if wave and stage indices are valid
+	// try extracting WavesConfig.Waves[CurrentWaveIndex].Stages[CurrentStageIndex]
+	// if it fails, throw an error
+	if (CurrentWaveIndex >= WavesConfig.Waves.Num())
+	{
+		UE_LOG(LogTemp, Error, TEXT("Invalid wave index: %d"), CurrentWaveIndex);
+		return;
+	}
+	if (CurrentStageIndex >= WavesConfig.Waves[CurrentWaveIndex].Stages.Num())
+	{
+		UE_LOG(LogTemp, Error, TEXT("Invalid stage index: %d"), CurrentStageIndex);
+		return;
+	}
+
+
+	WavesConfig = InWavesConfig;
+}
+
+
+void ASpawnManager::GetNextBatch(USpawnBatch* &Batch)
+{
+	// USpawnBatch* Batch = NewObject<USpawnBatch>();
+
+	bool bIsLastWave = CurrentWaveIndex >= WavesConfig.Waves.Num();
+	bool bIsLastStage = CurrentStageIndex >= WavesConfig.Waves[CurrentWaveIndex].Stages.Num();
+
+	Batch->StageConfig = WavesConfig.Waves[CurrentWaveIndex].Stages[CurrentStageIndex];
+	Batch->NextStageDelay = WavesConfig.Waves[CurrentWaveIndex].NextStageDelay;
+	Batch->DamageMultiplier = WavesConfig.Waves[CurrentWaveIndex].DamageMultiplier;
+	Batch->HealthMultiplier = WavesConfig.Waves[CurrentWaveIndex].HealthMultiplier;
+	Batch->SpeedMultiplier = WavesConfig.Waves[CurrentWaveIndex].SpeedMultiplier;
+	Batch->bIsLastStage = bIsLastStage;
+	Batch->bIsLastWave = bIsLastWave;
+
+	CurrentStageIndex++;
+
+	if (bIsLastStage)
+	{
+		CurrentWaveIndex++;
+		CurrentStageIndex = 0;
+	}
+}
---
+++ Source/BrackeyGame/Public/SpawnManager.h
@@ -0,0 +1,153 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "SpawnManager.generated.h"
+
+/**
+ * Maps an enemy class to a count
+ */
+USTRUCT()
+struct FUEnemyConfig : public FTableRowBase
+{
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Spawn Manager")
+	TSubclassOf<AActor> EnemyClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Spawn Manager")
+	int32 Count;
+
+	GENERATED_BODY()
+
+	FUEnemyConfig()
+	{
+		EnemyClass = nullptr;
+		Count = 0;
+	}
+};
+
+
+/**
+ * Contains a stage of enemies
+ */
+USTRUCT()
+struct FUSpawnStageConfig : public FTableRowBase
+{
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Spawn Manager")
+	TArray<FUEnemyConfig> Enemies;
+
+	GENERATED_BODY()
+
+	FUSpawnStageConfig()
+	{
+		Enemies.Empty();
+	}
+};
+
+/**
+ * Contains a wave of enemies with difficulty modifiers
+ */
+USTRUCT()
+struct FUSpawnWaveConfig : public FTableRowBase
+{
+	UPROPERTY(EditAnywhere, Category="Spawn Manager")
+	TArray<FUSpawnStageConfig> Stages;
+
+	UPROPERTY(EditAnywhere, Category = "Spawn manager")
+	float NextStageDelay;
+
+	UPROPERTY(EditAnywhere, Category = "Spawn manager")
+	float DamageMultiplier;
+
+	UPROPERTY(EditAnywhere, Category = "Spawn manager")
+	float HealthMultiplier;
+
+	UPROPERTY(EditAnywhere, Category = "Spawn manager")
+	float SpeedMultiplier;
+
+	GENERATED_BODY()
+
+	FUSpawnWaveConfig()
+	{
+		Stages.Empty();
+		NextStageDelay = 0.0f;
+		DamageMultiplier = 1.0f;
+		HealthMultiplier = 1.0f;
+		SpeedMultiplier = 1.0f;
+	}
+};
+
+
+/**
+ * Contains a list of waves
+ */
+USTRUCT()
+struct FUWavesConfig : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category="Spawn Manager")
+	TArray<FUSpawnWaveConfig> Waves;
+
+	FUWavesConfig()
+	{
+		Waves.Empty();
+	}
+};
+
+
+UCLASS(Blueprintable, BlueprintType)
+class USpawnBatch : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	// Default constructor
+	USpawnBatch();
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Spawn Manager")
+	FUSpawnStageConfig StageConfig;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Spawn manager")
+	float NextStageDelay;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Spawn manager")
+	float DamageMultiplier;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Spawn manager")
+	float HealthMultiplier;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Spawn manager")
+	float SpeedMultiplier;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Spawn manager")
+	bool bIsLastStage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Spawn manager")
+	bool bIsLastWave;
+};
+
+
+/**
+ * Contains waves of enemies for a level
+ */
+UCLASS()
+class BRACKEYGAME_API ASpawnManager : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ASpawnManager();
+
+private:
+	FUWavesConfig WavesConfig;
+	int CurrentWaveIndex;
+	int CurrentStageIndex;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Spawn manager")
+	void InitManager(const FUWavesConfig& InWavesConfig, int32 StartWaveIndex = 0, int32 StartStageIndex = 0);
+
+	UFUNCTION(BlueprintCallable, Category = "Spawn manager")
+	void GetNextBatch(USpawnBatch* &Batch);
+};
